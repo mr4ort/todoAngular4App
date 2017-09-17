e69a650bc6825d9c9958581447aa28ff4a46af6f
Igor <igor.datsenko@hotmail.com>
2017-09-18 00:39:06 +0300

    adds components;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoFormComponent } from './components/todos/todo-form/todo-form.component';
+import { TodoItemComponent } from './components/todos/todo-item/todo-item.component';
+import { TodoItemsComponent } from './components/todos/todo-items/todo-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    TodoItemComponent,
+    TodoItemsComponent
   ],
   imports: [
     BrowserModule
